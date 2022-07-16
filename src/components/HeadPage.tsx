8b8cc1864ee3c373f80b1ec1c6a703c2f5524ee9
Samuel Claudino <samuelcs131@gmail.com>
2022-07-16 03:50:09 -0300

    implementacao de SEO

--- src/components/HeadPage.tsx
+++ src/components/HeadPage.tsx
@@ -5,6 +5,17 @@ const HeadPage = ({titlePage}: iHeadPage): JSX.Element => {
     return(<>
     <Head>
         <title>{titlePage + ' | NextFit'}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+
+        {/* SEO */}
+        <meta name="description" content="nextfit"/>
+        <meta name="keywords" content="nextfit"/>
+        <meta name="author" content="Samuel Claudino"/>
+        <meta name="copyright" content="nextfit2022"/>
+        <meta name="theme-color" content="#2F2B54"/>
+        <meta httpEquiv="content-language" content="pt-br"/>
+        <meta property="og:title" content="nextfit"/>
+        <meta property="og:description" content="nextfit"/>
     </Head>
     </>)
 }
